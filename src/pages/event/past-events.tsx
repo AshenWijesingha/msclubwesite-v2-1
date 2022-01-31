2e5f39fbda548f75ac75a12a53ae9a077558faaf
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-01-31 22:32:42 +0530

    Get Events from Back End API (#67)

--- src/pages/event/past-events.tsx
+++ src/pages/event/past-events.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Slider from "react-owl-carousel";
-import events from '../../data/PastEventData.json';
 import { CARD_TYPE_EVENT, SLIDER_RESPONSIVE_BREAKPOINTS } from '../../constants';
 import { Card } from '../../components';
 import {translation} from '../../locales/en-US/translation.json';
+
+import { getPastEvets } from "../../api/EventAction";
+import { IEvent } from "../../interfaces/CardInterface";
 
 const PastEvents: React.FC = () => {
   let slider: any;
@@ -32,59 +34,72 @@ const PastEvents: React.FC = () => {
       slidePrev();
     }
   }
-  return (
-    <div className="container mt-3">
-      <h2 className="item-header">{translation.label["event-past-events-title"]}</h2>
-      <div className="item-navigation">
-        <div className="view-more-text">{translation.label["view-more"]}</div>
-        <div
-          onClick={slidePrev}
-          onKeyDown={slidePrevKeyBoard}
-          role="button"
-          tabIndex={0}
-          className="mr-2"
-        >
-          <i className="far fa-arrow-alt-circle-left fa-lg nav-icon" />
-        </div>
-        &nbsp;
-        <div
-          onClick={slideNext}
-          onKeyDown={slideNextKeyBoard}
-          role="button"
-          tabIndex={0}
-        >
-          <i className="far fa-arrow-alt-circle-right fa-lg nav-icon" />
-        </div>
-      </div>
 
-      {events && events.data.length > 0 && 
-      (
-        <Slider
-          className="owl-theme"
-          dots={false}
-          loop={true}
-          margin={70}
-          responsive={SLIDER_RESPONSIVE_BREAKPOINTS}
-          ref={(slide) => {
-            slider = slide;
-          }}
-        >
-          {events.data.map((event, index) => (
-            <Card 
-              key={index}
-              id={event.id}
-              title={event.title}
-              dateTime={event.dateTime}
-              description={event.description}
-              imageUrl={event.imageUrl}
-              link={event.link}
-              tags={event.tags}
-              type={CARD_TYPE_EVENT}
-            />
-          ))}
-        </Slider>
-      )}
-    </div>
+  const [pastEvents, getPastEvents] = useState<IEvent[]>();
+
+  useEffect(() => {
+    getPastEvents()
+        .then((response) => {
+          getPastEvents(response.data);
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+  }, []);
+
+  return (
+      <div className="container mt-3">
+        <h2 className="item-header">{translation.label["event-past-events-title"]}</h2>
+        <div className="item-navigation">
+          <div className="view-more-text">{translation.label["view-more"]}</div>
+          <div
+              onClick={slidePrev}
+              onKeyDown={slidePrevKeyBoard}
+              role="button"
+              tabIndex={0}
+              className="mr-2"
+          >
+            <i className="far fa-arrow-alt-circle-left fa-lg nav-icon" />
+          </div>
+          &nbsp;
+          <div
+              onClick={slideNext}
+              onKeyDown={slideNextKeyBoard}
+              role="button"
+              tabIndex={0}
+          >
+            <i className="far fa-arrow-alt-circle-right fa-lg nav-icon" />
+          </div>
+        </div>
+
+        {events && events.data.length > 0 &&
+        (
+            <Slider
+                className="owl-theme"
+                dots={false}
+                loop={true}
+                margin={70}
+                responsive={SLIDER_RESPONSIVE_BREAKPOINTS}
+                ref={(slide) => {
+                  slider = slide;
+                }}
+            >
+              {events.data.map((event, index) => (
+                  <Card
+                      key={index}
+                      id={event.id}
+                      title={event.title}
+                      dateTime={event.dateTime}
+                      description={event.description}
+                      imageUrl={event.imageUrl}
+                      link={event.link}
+                      tags={event.tags}
+                      type={CARD_TYPE_EVENT}
+                  />
+              ))}
+            </Slider>
+        )}
+      </div>
   );
 };
 
